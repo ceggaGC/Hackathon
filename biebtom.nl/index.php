f05145ae29699631d73dbd5d5c620c299418243f
goran cederburg <16071@ma-web.nl>
2014-08-27 11:13:05 +0200

    opmaak pagina

--- biebtom.nl/index.php
+++ biebtom.nl/index.php
@@ -1,9 +1,20 @@
 <html>
-<link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
 <body>
-<div id="search-bar">
-<div id="search"><input class="searchfield" type="text" name="search" id="search" /></div>
-</div>
-<div id="page"></div>
+    <div id="search-bar">
+        <div id="search"><input class="searchfield" type="text" name="search" id="search" /></div>
+    </div>
+    <div id="logobar">
+        <img id="logo" src="images/logo.png">
+    </div>
+   
+    <div id="page">
+         <div id="opdrachten-lijst">
+        <div id="kop">opdrachten</div><br>
+        opdracht 1 <br>
+        opdracht 2 <br>
+        opdracht 3 <br>
+    </div>
+    </div>
 </body>
 </html>
