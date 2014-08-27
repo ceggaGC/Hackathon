bf9c1099eb36413aa7bd50bd86b7f39517aa2bf1
Ringo <rcontent@ringocontent.com>
2014-08-27 10:07:39 +0200

    voorbeeld website toegevoegd

---
+++ biebtom.nl/index.php
@@ -0,0 +1,9 @@
+<html>
+<link rel="stylesheet" type="text/css" href="style.css">
+<body>
+<div id="search-bar">
+<div id="search"><input class="searchfield" type="text" name="search" id="search" /></div>
+</div>
+<div id="page"></div>
+</body>
+</html>
